c2bda2d90bbd518edea7bd4b0fc4baa6927ab1ae
Тарасевич Василий <taravask@trilobitesoft.com>
2019-01-14 02:41:25 +0300

    added textures for Player and Platforms, added Physic Materials for Player and Platforms, created prefabs for Platforms, added infinity jump behaviour for Player

---
+++ JumpQuest/Assets/Scripts/PlayerJump.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerJump : MonoBehaviour
+{
+    [SerializeField]
+    private Rigidbody2D m_Rigidbody = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.name == "Platform")
+        {
+            Jump();
+        }
+    }
+
+    void Jump()
+    {
+        m_Rigidbody.AddForce(new Vector3(0, 300, 0));
+    }
+}
